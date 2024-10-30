af9191f318b6e29de9943c46a18f38f925c53b92
mario <khantkokozaw382000@gmail.com>
2024-10-30 17:19:43 +0800

    add Navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { BsBugFill } from "react-icons/bs";
+
+const Navbar = React.memo(() => {
+  const current_path = usePathname();
+
+  const nav_links = [
+    { label: "Dashboard", link: "/" },
+    { label: "Issues", link: "/issues" },
+  ];
+
+  return (
+    <nav className="flex space-x-6 h-14 items-center border-b mb-5 px-5">
+      <Link href={"/"}>
+        <BsBugFill />
+      </Link>
+      <ul className="flex space-x-6">
+        {nav_links.map((nlink) => (
+          <Link
+            key={nlink.link}
+            className={classNames({
+              "text-zinc-900": current_path === nlink.link,
+              "text-zinc-500": current_path !== nlink.link,
+              "hover:text-zinc-900 cursor-pointer font-bold transition-colors":
+                true,
+            })}
+            href={nlink.link}
+          >
+            {nlink.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+});
+
+export default Navbar;
